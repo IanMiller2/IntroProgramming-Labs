3d8c89a8ba307385966dde42aeb29e9947fc8eb9
Ian Miller <ian.miller2@marist.edu>
2018-09-14 12:04:57 -0400

    Solution to Mad Libs Exercise.

---
+++ madlib.py
@@ -0,0 +1,9 @@
+noun = input("Enter a noun")
+adjective = input("Enter an adjective")
+verb = input("Enter a verb")
+place = input("Enter a place")
+print("The", adjective, noun, verb, place, "at an incredible rate of speed")
+
+
+
+
