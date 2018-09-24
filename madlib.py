81acdc556e9352edc0a87a71ff4286024fda195f
Ian Miller <ian.miller2@marist.edu>
2018-09-24 19:27:10 -0400

    Solution to Mad Libs exercise.

--- madlib.py
+++ madlib.py
@@ -1,8 +1,23 @@
-noun = input("Enter a noun")
-adjective = input("Enter an adjective")
-verb = input("Enter a verb")
-place = input("Enter a place")
-print("The", adjective, noun, verb, place, "at an incredible rate of speed")
+# CMPT 120L 113
+# Mad Lib Program
+# Author: Ian Miller
+# Created: 14 Sep 2018
+
+def promptForWords():
+    global noun, adjective, verb, place
+    noun = input("Enter a noun: ")
+    adjective = input("Enter an adjective: ")
+    verb = input("Enter a verb: ")
+    place = input("Enter a place: ")
+
+def makeAndPrintSentence():
+    print("The", adjective, noun, verb, "to", place, "at an incredible rate of speed")
+
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+main()
 
 
 
