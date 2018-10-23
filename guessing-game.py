c75cfaa123cdbcefe3a16dcda10e6fa915bfcbfd
Ian Miller <ian.miller2@marist.edu>
2018-10-23 02:15:46 -0400

    Usability improvements to the game, resolves #3 and #4

--- guessing-game.py
+++ guessing-game.py
@@ -7,8 +7,13 @@ def main():
         guess = input("Enter the name of an animal: ").strip().lower()
         if guess == ans:
             print("\nCongratulations, you win!")
+            feedback = input("\nDo you like this animal? Enter 'y' or 'n': ").strip().lower()
+            if feedback == "y":
+                print("\nI'm glad. Thank you for playing!")
+            elif feedback == "n":
+                print("\nI'm sorry you feel that way. Thank you for playing!")
             break
-        elif guess == "quit":
+        elif guess[0] == "q" : 
             print("\nThank you for playing!")
             break
         else:
